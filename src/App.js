8cafd7fe8e93b956f53a3eb3a0d7a63b0fe16889
cby3149 <boyuan.eth@gmail.com>
2024-07-25 12:17:04 -0700

    Simple html

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{ fontFamily: 'Poppins, sans-serif', fontSize: 70, fontWeight: 500, color: 'black'}}>
+          OpenMind.
+        </p>
+        <a
+          href='mailto:ask@openmind.ai'
+          style={{ fontFamily: 'Poppins, sans-serif', color: 'grey', style: 'none', fontSize: 18 }}
+        >
+          ask@openmind.ai
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
